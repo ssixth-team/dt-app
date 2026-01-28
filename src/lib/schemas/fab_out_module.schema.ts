404e261af109556ef21f566a9d06c57291e90a76
pjs2ung <93852237+pjs2ung@users.noreply.github.com>
2026-01-28 10:48:44 +0900

    Create fab_out_module.schema.ts

---
+++ src/lib/schemas/fab_out_module.schema.ts
@@ -0,0 +1,16 @@
+import { type Account } from './account';
+import type { MPW_FAB_OUT_MODULE_WAFER } from '$lib/schemas/fab_out_module_wafer.schema';
+import type { MPW_ASM_DECISION_TYPE } from '$lib/schemas/asm_decision_type.schema';
+
+export interface MPW_FAB_OUT_MODULE {
+  id: number;
+  childId: number;
+  wafers: [MPW_FAB_OUT_MODULE_WAFER]
+  module: string;
+  ipOwner: [Account];
+  asmDecisionType : MPW_ASM_DECISION_TYPE;
+  asmDecisionUser: Account;
+  asmDecisionDate: string;
+}
+
+export const fabOutModuleSchema = '++id, &[childId+wafer+module]';
