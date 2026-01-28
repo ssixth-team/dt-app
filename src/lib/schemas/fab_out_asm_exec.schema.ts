b59482488a9bd85924c3f251eaddc31ce317bad8
pjs2ung <93852237+pjs2ung@users.noreply.github.com>
2026-01-28 10:48:13 +0900

    Create fab_out_asm_exec.schema.ts

---
+++ src/lib/schemas/fab_out_asm_exec.schema.ts
@@ -0,0 +1,22 @@
+import { type Account } from './account';
+import type { MPW_FAB_OUT_ASM_DECISION } from '$lib/schemas/fab_out_asm_decision.schema';
+
+export interface MPW_FAB_OUT_ASM_EXEC {
+  id: number;
+  fabOutId: number;
+  asmPartId: string;
+  asmSite: string;
+  pkgType: string;
+  edsPartId: string;
+  asmType: string;
+  asmIpOwner: [Account];
+  ipName: string;
+  backLapSize: string;
+  createUser: Account;
+  createDate: string;
+  updateUser: Account;
+  updateDate: string;
+  decisions: [MPW_FAB_OUT_ASM_DECISION];
+}
+
+export const fabOutAsmExecSchema = '++id';
